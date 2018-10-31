5d8a8b74cbbbeca48b8583eb99276dcd94a018e2
marty-suzuki <s1180183@gmail.com>
2018-11-01 02:31:37 +0900

    fix project

---
+++ ReverseExtension/ReverseExtension.h
@@ -0,0 +1,22 @@
+//
+//  ReverseExtension.h
+//  ReverseExtension
+//
+//  Created by marty-suzuki on 2018/11/01.
+//  Copyright © 2018年 marty-suzuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DelegateTransporter.h"
+#import "ExceptionHandler.h"
+
+//! Project version number for ReverseExtension.
+FOUNDATION_EXPORT double ReverseExtensionVersionNumber;
+
+//! Project version string for ReverseExtension.
+FOUNDATION_EXPORT const unsigned char ReverseExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReverseExtension/PublicHeader.h>
+
+
